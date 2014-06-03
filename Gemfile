1109d10223d3863c78fa648ec76b8c816e1634f8
Jose Sanchez <hailex@live.com>
2014-06-03 16:04:01 +0000

    Implemented policies with Pundit, completing/deleting tasks

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ end
 
 gem 'devise'
 gem 'figaro'
+gem 'pundit'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,14 @@
 class Task < ActiveRecord::Base
   belongs_to :user
   validates :description, length: {minimum: 2}, presence: true
+  default_scope {order("expires_at ASC")}
+
+  def completed?
+    status = "Expired"
+    if self.completed == true
+      status = "Completed"
+    end
+    status
+  end
+
 end
